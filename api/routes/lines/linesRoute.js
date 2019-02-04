ed777cbe69b30c2bcf567aab9cc5a9e161a12ebc
Addison-Stavlo <AddieStavlo@gmail.com>
2019-02-04 16:32:46 -0500

    get by date and post methods added

--- api/routes/lines/linesRoute.js
+++ api/routes/lines/linesRoute.js
@@ -28,7 +28,7 @@ router.get("/", authenticate, async (req, res) => {
   let lines = await db("lines")
     .join("users", "users.id", "=", "lines.user_id")
     .where({ "users.username": req.decoded.username })
-    .select("line", "date");
+    .select("line", "date", "lines.id");
 
   res.status(200).json({ lines });
 });
@@ -39,5 +39,44 @@ router.get("/test", authenticate, async (req, res) => {
   res.status(200).json({ decoded });
 });
 
-router.get("/:date", authenticate, async (req, res) => {});
+// TODO - get route for 10yr history by date
+// router.get("/history/:date", authenticate, async (req, res) => {
+//   let dateArr = req.params.date.split("-");
+//   let year = dateArr.shift();
+//   let monthAndDay = dateArr.join("-");
+//   let datesToFind = [];
+
+//   let lines = await db("lines")
+//     .join("users", "users.id", "=", "lines.user_id")
+//     .where({ "users.username": req.decoded.username })
+//     .whereIn("lines.date", [monthAndDay])
+//     .select("line", "date", "lines.id");
+
+//   res.status(200).json(lines);
+// });
+
+router.get("/:date", authenticate, async (req, res) => {
+  let lines = await db("lines")
+    .join("users", "users.id", "=", "lines.user_id")
+    .where({ "users.username": req.decoded.username })
+    .where({ "lines.date": req.params.date })
+    .select("line", "date", "lines.id")
+    .first();
+
+  res.status(200).json(lines);
+});
+
+router.post("/", authenticate, async (req, res) => {
+  //todo just put userID on the token...
+  let { id } = await db("users")
+    .where({ username: req.decoded.username })
+    .first();
+
+  let lineIDs = await db("lines").insert({ ...req.body, user_id: id });
+
+  res
+    .status(201)
+    .json({ message: "line created successfully", id: lineIDs[0] });
+});
+
 module.exports = router;
--- notes.md
+++ notes.md
@@ -8,3 +8,15 @@ POST - body - {username, password}
 note: username key will work with both matching username or email value.
 
 Response: {message, username, token}
+
+'.../api/lines'
+GET - headers - {Authorization: token}
+
+Response: {lines: [ {line, date} ] }
+
+'.../api/lines/YYYY-MM-DD'
+GET - headers - {Authorization: token}
+
+Response: {line, date}
+
+note: (please use leading 0s for days/months 1-9)
